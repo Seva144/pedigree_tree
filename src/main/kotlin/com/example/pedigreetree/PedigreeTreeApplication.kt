8f491ec47a91461703144a4145f85fe9a2f23dd0
Public2024 <public2024@internet.ru>
2024-04-30 21:51:52 +0300

    Start

---
+++ src/main/kotlin/com/example/pedigreetree/PedigreeTreeApplication.kt
@@ -0,0 +1,12 @@
+package com.example.pedigreetree
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PedigreeTreeApplication
+
+fun main(args: Array<String>) {
+	runApplication<PedigreeTreeApplication>(*args)
+
+}
